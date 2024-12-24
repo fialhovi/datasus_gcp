3505654613160b5d437292c1755379cdaa24e2aa
VINICIUS FIALHO <vinicius.fialho96@gmail.com>
2024-12-24 19:54:19 -0300

    add example of sheets_to_bigquery

---
+++ src/sheets_to_bigquery.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+
+from classes.cloud.GoogleCloud import GoogleCloud
+
+secret_project_id = os.getenv("secret_project_id")
+secret_id = os.getenv("secret_id")
+
+df = pd.read_csv("./data/csv_lookup/lookup_municipality.csv", index_col=False)
+df = df.astype(str)
+df["date_loading"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+google_cloud = GoogleCloud()
+
+# sa_json = "config/service_account.json"
+sa_json = google_cloud.access_secret_from_secret_manager(secret_project_id, secret_id)
+
+google_cloud.insert_dataframe_into_bigquery(
+    df,
+    table_id="lookup.tb_lookup_municipality",
+    sa_json=sa_json,
+    partition_columns="",
+    gcp_project="datasus-prod",
+)
